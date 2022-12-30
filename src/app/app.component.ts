f65a14a1784d63cd7658c2c7f573dd27cd9b4cd6
Andrés Castillo <andres.castillo@acastillo.co>
2022-12-29 19:58:18 -0500

    todo app tasks list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'todo-app';
+
+  tasksList: string[] = ['texto tarea 1', 'texto tarea 2', 'texto tarea 3'];
 }
